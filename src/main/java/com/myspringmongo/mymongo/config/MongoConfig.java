1be2d73112d3b1dc28f5c19573585376beeca269
davon <747664918@qq.com>
2022-01-22 22:56:14 +0800

    Added a test commit

--- src/main/java/com/myspringmongo/mymongo/config/MongoConfig.java
+++ src/main/java/com/myspringmongo/mymongo/config/MongoConfig.java
@@ -10,6 +10,7 @@ public class MongoConfig {
 
     @Bean
     public MongoTemplate getMongoTemplate() {
+        //this is test
         return new MongoTemplate(new SimpleMongoClientDatabaseFactory("mongodb://admin:admin@localhost:27017/davon01?authSource=admin"));
     }
 }
