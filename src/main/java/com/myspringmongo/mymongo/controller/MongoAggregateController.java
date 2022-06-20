f098c6d2f507959ea8df60bcfed1070e9db8a22d
davon <747664918@qq.com>
2022-06-20 22:00:56 +0800

    Add test data

--- src/main/java/com/myspringmongo/mymongo/controller/MongoAggregateController.java
+++ src/main/java/com/myspringmongo/mymongo/controller/MongoAggregateController.java
@@ -39,32 +39,7 @@ public class MongoAggregateController {
     }
 
     @RequestMapping("/date/avg")
-    public String getByDateAndAVG() throws ParseException, JsonProcessingException {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        Date start = dateFormat.parse("2020-01-30");
-        Date end = dateFormat.parse("2022-01-30");
-        Criteria criteria = Criteria.where("date").gte(start).lt(end);
-
-        DateOperators.DateOperatorFactory dateOperatorFactory = DateOperators.dateOf("date");
-        DateOperators.DateToString dateToString = dateOperatorFactory.toString("yyyy-MM-dd");
-
-//        ProjectionOperation.ProjectionOperationBuilder and = Aggregation.project("date").and(DateOperators.DateToString.dateOf("date").toString("%Y-%m-%d"));
-        GroupOperation groupMaxOperation = Aggregation.group().sum("price").as("price")
-                .avg("quantity").as("avgQuantity");
-        ReplaceRootOperation replaceRootOperation = Aggregation.replaceRoot(dateToString);
-        ProjectionOperation as = Aggregation.project("date").and(dateToString).as("groupdate");
-        AggregationResults<Document> aggregate = mongoTemplate.aggregate(Aggregation.newAggregation(new MatchOperation(criteria), replaceRootOperation, groupMaxOperation), "orders", Document.class);
-        List<Document> mappedResults = aggregate.getMappedResults();
-
-        GroupOperation group = Aggregation.group(dateToString.toDocument().toString());
-
-        Aggregation.newAggregation(new MatchOperation(criteria), group, groupMaxOperation);
-        return mapper.writeValueAsString(mappedResults);
-    }
-
-
-    @RequestMapping("/date/avg/1")
-    public String getByDateAndAVGAnd1() throws ParseException, JsonProcessingException {
+    public String groupByDateAndAggregate() throws ParseException, JsonProcessingException {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         Date start = dateFormat.parse("2020-01-30");
         Date end = dateFormat.parse("2022-01-30");
@@ -76,7 +51,6 @@ public class MongoAggregateController {
 
         List<AggregationOperation> operations = new ArrayList<>();
 
-//        ProjectionOperation groupDate = Aggregation.project().andExpression("{ $dateToString: { format: \"%Y-%m-%d\", date: \"$date\" } }").as("groupDate").and("price").as("price").and("quantity").as("quantity");
         ProjectionOperation groupDate = Aggregation.project().andExpression(dateToString.toDocument().toJson()).as("groupDate").and("price").as("price").and("quantity").as("quantity");
         GroupOperation groupMaxOperation = Aggregation.group("groupDate").sum("price").as("totalPrice")
                 .avg("quantity").as("avgQuantity");
